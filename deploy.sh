0912410da728f36c217c34139435517327ebd848
Sam Merrell <merrell.sam@gmail.com>
2017-10-26 23:01:53 -0600

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,15 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo -t hugo-cactus-theme # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+if [ $? -ne 0 ]; then
+    echo -e "\033[0;31mDeploy failed!\033[0m"
+    exit 1
+fi
+
 # Go To Public folder
 cd public
 # Add changes to git.
-#git add .
+git add .
 
 # Commit changes.
 msg="Rebuild site `date`"
@@ -16,7 +21,7 @@ if [ $# -eq 1 ]
     then msg="$1"
 fi
 
-echo git commit -m "$msg"
+git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
